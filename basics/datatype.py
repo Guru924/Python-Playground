71e78f91ed33095a5f7f5da93fa8975c3d235490
Guru924 <guruprasaddas924@gmail.com>
2024-09-10 22:25:25 +0530

    Datatype in py

---
+++ basics/datatype.py
@@ -0,0 +1,23 @@
+# 1. Numeric Data: int, float, complex
+# * float 7.349
+# * int 3,4,0
+# * complex 6+3i
+
+# 2. Text data: str
+# str: "hello", "python"
+
+# 3. Boolean data:
+# false, true
+
+# 4. sequenced data: list, tuple
+# list1 = {{1,2,3,4,5}, {'apple', 'banana'}}
+# tuple1 = {{'apple', 'banana'}, {'cat', 'rat'}}
+# list mutable and tuple is immutable
+
+#5 Mapped data: dict
+# dist = {"name": "Guru", "surename": "Prasad Das", "age": 20}
+
+a = None
+print(a,type(a))
+
+# everthing in python is an object
