73d8989b953e276f2e8a67348ba8df34756c02ec
Guru924 <guruprasaddas924@gmail.com>
2024-09-10 22:26:25 +0530

    typecasting in python

---
+++ basics/typeCast.py
@@ -0,0 +1,17 @@
+# a = "1"
+# b = 2
+# print(a+b) # error (can't concat string and number)
+
+# a = 1
+# b = int("guru") # error (provide value int)
+# print(type(int))
+
+a = 1
+b = int("2")
+print(a+b)
+
+#implicit typecasting
+
+# c = 1.9
+# d = 8
+# print(c+d) #float
