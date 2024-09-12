294edd6d3731325cdc5fc383865361ce0f2e3f55
Guru924 <guruprasaddas924@gmail.com>
2024-09-12 18:33:13 +0530

    Tuples in python

---
+++ basics/tuples.py
@@ -0,0 +1,10 @@
+tup = (1,5, "green") #are unchangeable
+print(type(tup), tup)
+
+tup1 = (1)
+print(type(tup1),tup1)
+tup2 = (1,)
+print(type(tup2),tup2)
+
+
+# all method are same of lists in tuple
