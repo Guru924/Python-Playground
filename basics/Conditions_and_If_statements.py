e1b4c32ee1b2a9405b78c7fdb629164b89950731
Guru924 <guruprasaddas924@gmail.com>
2024-09-26 09:59:22 +0530

    short hand if else

--- basics/Conditions_and_If_statements.py
+++ basics/Conditions_and_If_statements.py
@@ -30,6 +30,9 @@ print("A") if a > b else print("B")
 a = 330
 b = 330
 print("A") if a > b else print("=") if a == b else print("B")
+print(9) if a>b else ""
+c = 9 if a>b else 0
+print(c)
 
 
 a = 33
